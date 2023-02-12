429f4992a44733cdd778fd6fd9af99cb47b23a6b
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-02-12 17:31:26 +0800

    :lipstick: 重写顶栏样式
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Client.UI/src/components/TopBar.vue
+++ src/Shimakaze.Client.UI/src/components/TopBar.vue
@@ -3,16 +3,18 @@
     <div class="avatar">
       <img src="/avatar.jpg" alt="avatar">
     </div>
+
     <div class="level">
       14
     </div>
+
     <div class="info">
-      <div class="account">
+      <span class="account">
         挖坑狂魔岛风酱!
-      </div>
-      <div class="desc">
+      </span>
+      <span class="desc">
         只管挖，不管填!
-      </div>
+      </span>
     </div>
   </div>
 </template>
@@ -20,43 +22,83 @@
 <script lang="ts" setup>
 
 </script>
+
 <style lang="scss">
 @use "sass:color";
 @use "@/styles/theme.scss";
 
 .top-bar {
+  height: 2rem;
+  width: 100vw;
+  max-height: 16rem;
   display: flex;
-  gap: 1rem;
+  position: fixed;
   background-color: #{color.change(theme.$sc-neutral-tertiary, $alpha: 0.5)};
+  box-sizing: content-box;
+  justify-content: left;
   align-items: center;
+  gap: 1rem;
+  transition: all .25s ease;
+
+  &:hover {
+    height: 8%;
+
+    .level {
+      padding: 0.25rem;
+      border-style: solid;
+      border-width: 4px;
+      border-color: #{color.change(theme.$sc-neutral-primary, $alpha: 0.5)};
+      background-color: #{color.change(theme.$sc-neutral-tertiary-alt, $alpha: 0.5)};
+    }
+
+    .info {
+      .desc {
+        display: unset;
+        opacity: unset;
+        position: unset;
+      }
+    }
+  }
 
   .avatar {
-    display: inline-flex;
+    display: inline-block;
+    height: 100%;
+    aspect-ratio: 1 / 1;
+    box-sizing: border-box;
     border-style: solid;
     border-width: 4px;
     border-color: #{color.change(theme.$sc-neutral-primary, $alpha: 0.5)};
 
     img {
-      width: 4rem;
-      height: 4rem;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
   }
 
   .level {
-    padding: 0.25rem;
     border-radius: 50%;
-    border-style: solid;
-    border-width: 4px;
-    border-color: #{color.change(theme.$sc-neutral-primary, $alpha: 0.5)};
-    background-color: #{color.change(theme.$sc-neutral-tertiary-alt, $alpha: 0.5)};
-    font-weight: bold;
-    font-size: large;
+    transition: all .25s ease;
   }
 
   .info {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: all .25s ease;
+
     .account {
       font-weight: bold;
-      font-size: larger;
+      transition: all .25s ease;
+    }
+
+    .desc {
+      display: hidden;
+      position: absolute;
+      opacity: 0;
+      transition: opacity .25s ease;
     }
   }
 }
