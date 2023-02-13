bdb60ea28240305eaf1ae03ff2efa41cc5bef5cd
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-02-14 07:43:23 +0800

    update
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Client.UI/src/components/SCCampaignList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div flex>
+    <div
+      v-for="item, index in items"
+      :key="index"
+      @click="$emit('click', item)"
+    >
+      <img
+        :src="item.icon"
+        :alt="item.name"
+        w-4
+        h-4
+      >
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { CampaignInfo } from '@m/CampaignInfo'
+
+export interface SCCampaignListProps{
+  items: CampaignInfo[]
+}
+export interface SCCampaignListEmits{
+  (e:'click', campaign: CampaignInfo): void
+}
+
+defineProps<SCCampaignListProps>()
+defineEmits<SCCampaignListEmits>()
+
+</script>
---
+++ src/Shimakaze.Client.UI/src/models/CampaignInfo.ts
@@ -0,0 +1,5 @@
+export interface CampaignInfo {
+  id: string
+  name: string
+  icon: string
+}
