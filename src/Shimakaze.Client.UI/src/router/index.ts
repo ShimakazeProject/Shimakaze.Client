63d341bba38ff972209302bbc1698b2e28bc2f1e
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-02-28 23:47:18 +0800

    :lipstick: 跳页动画
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Client.UI/src/router/index.ts
+++ src/Shimakaze.Client.UI/src/router/index.ts
@@ -7,3 +7,17 @@ export const router = createRouter({
   history: createMemoryHistory(),
   routes
 })
+
+router.afterEach((to, from) => {
+  let transition: string
+  if (from.fullPath === to.fullPath) {
+    transition = 'reload'
+  } else if (to.fullPath === '/') {
+    transition = 'pop'
+  } else {
+    const toDepth = to.path.split('/').length
+    const fromDepth = from.path.split('/').length
+    transition = toDepth < fromDepth ? 'pop' : 'push'
+  }
+  to.meta.transition = transition
+})
