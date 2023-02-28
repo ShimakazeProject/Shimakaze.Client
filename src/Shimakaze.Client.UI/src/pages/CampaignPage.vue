4e0aa979c1f74781e9edd270d747a8ee10e06516
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-02-28 23:49:03 +0800

    :sparkles: 添加新页面
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Client.UI/src/pages/CampaignPage.vue
@@ -0,0 +1,12 @@
+<template path="/campaign">
+<ClientButton
+  content="back"
+  @click="router.back()"
+/>
+</template>
+
+<script lang="ts" setup>
+import ClientButton from '@/components/ClientButton.vue'
+import { router } from '@/router'
+
+</script>
--- src/Shimakaze.Client.UI/src/pages/HomePage.vue
+++ src/Shimakaze.Client.UI/src/pages/HomePage.vue
@@ -2,11 +2,13 @@
 <ClientButton
   v-for="button, index in buttons" :key="index"
   :content="button.name"
+  @click="router.push(button.path)"
 />
 </template>
 
 <script lang="ts" setup>
 import ClientButton from '@/components/ClientButton.vue'
+import { router } from '@/router'
 import { getMainMenus } from '@/services/main'
 import { onMounted, ref } from 'vue'
 /* global Services */
