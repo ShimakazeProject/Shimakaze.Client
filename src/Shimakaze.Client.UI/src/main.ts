2354d52996a72d48da4d36c35f6238f114280b3b
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-03-21 15:34:08 +0800

    :technologist: Vue 调试工具
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Client.UI/src/main.ts
+++ src/Shimakaze.Client.UI/src/main.ts
@@ -3,4 +3,10 @@ import { router } from '@/router'
 import App from '@/App.vue'
 import '@/styles/global.sass'
 
+if (import.meta.env.MODE === 'development') {
+  await import('@vue/devtools').then(i => {
+    i.default.connect(/* host, port */)
+  })
+}
+
 createApp(App).use(router).mount('#app')
