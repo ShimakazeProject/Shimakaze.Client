295d857b63dcccf19ed6a2bcabde8833dc55dddd
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-03-22 17:05:45 +0800

    :bug: 修改main
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Client.UI/src/main.ts
+++ src/Shimakaze.Client.UI/src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import { router } from '@/router'
 import App from '@/App.vue'
+import { router } from '@/routers'
 import '@/styles/global.sass'
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
+import { createApp } from 'vue'
 
 if (import.meta.env.MODE === 'development') {
   await import('@vue/devtools').then(i => {
@@ -9,4 +11,10 @@ if (import.meta.env.MODE === 'development') {
   })
 }
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(
+    createPinia()
+      .use(piniaPersist)
+  )
+  .use(router)
+  .mount('#app')
