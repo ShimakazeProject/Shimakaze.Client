aab9b2bde51f79a2b6405c2a4ee04608eb237fce
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-03-26 12:38:48 +0800

    :sparkles: 视频背景 根据操作系统决定是否圆角
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Client.UI/src/Background.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="background">
+  <video autoplay loop src="/background.mp4">
+    <picture>
+      <source srcset="/background.png" type="image/png">
+      <img src="/background.png" alt="background">
+    </picture>
+  </video>
+</div>
+</template>
+
+<style lang="scss">
+.background {
+  height: 100vh;
+  position: absolute;
+  width: 100vw;
+  z-index: -9999;
+
+  video,
+  picture,
+  picture > img {
+    height: 100vh;
+    object-fit: cover;
+    width: 100vw;
+  }
+
+  [rounded] &,
+  [rounded] & video {
+    border-radius: 8px;
+    overflow: hidden;
+  }
+}
+</style>
--- src/Shimakaze.Client.UI/src/main.ts
+++ src/Shimakaze.Client.UI/src/main.ts
@@ -1,9 +1,11 @@
 import App from '@/App.vue'
+import Background from '@/Background.vue'
 import { router } from '@/routers'
 import '@/styles/global.scss'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
+import { platform, version } from '@tauri-apps/api/os'
 
 if (import.meta.env.MODE === 'development') {
   await import('@vue/devtools').then(i => {
@@ -11,6 +13,14 @@ if (import.meta.env.MODE === 'development') {
   })
 }
 
+const platformName = await platform()
+const osVersion = await version()
+console.log(platformName, osVersion)
+if (platformName === 'win32' && Number(osVersion.split('.')[2]) > 22000) {
+  document.querySelector('body')?.setAttribute('rounded', '')
+}
+
+createApp(Background).mount('#background')
 createApp(App)
   .use(
     createPinia()
