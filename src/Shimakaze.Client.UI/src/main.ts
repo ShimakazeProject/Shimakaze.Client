f898365af859bad4830b5d91b1558fbc8e1f0c74
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-03-26 13:02:13 +0800

    :fire: 移除一个不必要的内容
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Client.UI/src/main.ts
+++ src/Shimakaze.Client.UI/src/main.ts
@@ -1,27 +1,39 @@
-import App from '@/App.vue'
 import Background from '@/Background.vue'
 import { router } from '@/routers'
+import { init } from '@/services/i18n'
 import '@/styles/global.scss'
+import { platform, version } from '@tauri-apps/api/os'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
-import { platform, version } from '@tauri-apps/api/os'
+import { RouterView } from 'vue-router'
+import { system } from './apis/kernel'
 
 if (import.meta.env.MODE === 'development') {
   await import('@vue/devtools').then(i => {
     i.default.connect(/* host, port */)
   })
+
+  system()
+    .then(console.log)
+    .catch(console.error)
 }
 
-const platformName = await platform()
-const osVersion = await version()
-console.log(platformName, osVersion)
-if (platformName === 'win32' && Number(osVersion.split('.')[2]) > 22000) {
-  document.querySelector('body')?.setAttribute('rounded', '')
-}
+// TODO: 本地化翻译部分需要重构
+init('zh-Hans', '')
+  .catch(console.error)
+
+Promise
+  .all([platform(), version()])
+  .then(([platformName, osVersion]) => {
+    if (platformName === 'win32' && Number(osVersion.split('.')[2]) > 22000) {
+      document.querySelector('body')?.setAttribute('rounded', '')
+    }
+  })
+  .catch(console.error)
 
 createApp(Background).mount('#background')
-createApp(App)
+createApp(RouterView)
   .use(
     createPinia()
       .use(piniaPluginPersistedstate)
